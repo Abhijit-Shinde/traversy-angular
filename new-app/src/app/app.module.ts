1ddeb32dc09e63f9cd48b7243841b9f613d7aa1f
Abhijit-Shinde <shindeabhijit456@gmail.com>
2021-05-16 14:59:50 +0530

    about and footer

--- new-app/src/app/app.module.ts
+++ new-app/src/app/app.module.ts
@@ -13,9 +13,11 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
-  {path: '', component: TasksComponent}
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
 ]
 
 @NgModule({
@@ -26,7 +28,8 @@ const appRoutes: Routes = [
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
